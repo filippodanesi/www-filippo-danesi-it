c1267546549155b3d02fe9f6fe2078458cf85d5a
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-26 13:55:20 +0200

    Update chi-sono.js

--- pages/chi-sono.js
+++ pages/chi-sono.js
@@ -8,7 +8,7 @@ export async function getStaticProps() {
   return { props: { authorDetails } }
 }
 
-export default function About({ authorDetails }) {
+export default function Chi sono({ authorDetails }) {
   const { mdxSource, frontMatter } = authorDetails
 
   return (
