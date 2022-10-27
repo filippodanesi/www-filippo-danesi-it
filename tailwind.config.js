fc8e9011d14f66cfed1225f0d7444fdda8d83360
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-27 16:27:19 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.cyan,
+        primary: colors.sky,
         gray: colors.neutral,
       },
       typography: (theme) => ({
