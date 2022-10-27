f5bb188825c7f7b04b1b2efcae1b7ecda467dc57
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-27 15:49:37 +0200

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Certificazioni' },
+  { href: '/certificazioni', title: 'Certificazioni' },
   { href: '/about', title: 'About' },
 ]
 
