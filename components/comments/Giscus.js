338d426477d25f27aa05cd38eab260ff0207596c
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-29 16:04:41 +0200

    Update Giscus.js

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -32,16 +32,16 @@ const Giscus = () => {
 
     const script = document.createElement('script')
     script.src = 'https://giscus.app/client.js'
-    script.setAttribute('data-repo', filippodanesi/www-filippo-danesi-it)
-    script.setAttribute('data-repo-id', R_kgDOITfCxw)
-    script.setAttribute('data-category', General)
-    script.setAttribute('data-category-id', DIC_kwDOITfCx84CSM5o)
-    script.setAttribute('data-mapping', pathname)
-    script.setAttribute('data-reactions-enabled', 1)
-    script.setAttribute('data-emit-metadata', 0)
-    script.setAttribute('data-input-position', bottom)
-    script.setAttribute('data-lang', it)
-    script.setAttribute('data-theme', preferred_color_scheme)
+    script.setAttribute('data-repo', 'filippodanesi/www-filippo-danesi-it')
+    script.setAttribute('data-repo-id', 'R_kgDOITfCxw')
+    script.setAttribute('data-category', 'General')
+    script.setAttribute('data-category-id', 'DIC_kwDOITfCx84CSM5o')
+    script.setAttribute('data-mapping', 'pathname')
+    script.setAttribute('data-reactions-enabled', '1')
+    script.setAttribute('data-emit-metadata', '0')
+    script.setAttribute('data-input-position', 'bottom')
+    script.setAttribute('data-lang', 'it')
+    script.setAttribute('data-theme', 'preferred_color_scheme')
     script.setAttribute('crossorigin', 'anonymous')
     script.async = true
 
