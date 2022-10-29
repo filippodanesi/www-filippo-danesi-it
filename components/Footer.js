01d3035cbb2e3e74a621927c3819e5456d79cb15
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-29 15:36:15 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -20,7 +20,7 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="/">{` Privacy Policy `}</Link>
-          <div>{` • `}</div>
+          <div>{` | `}</div>
           <Link href="/">{` Cookie Policy `}</Link>
         </div>
       </div>
