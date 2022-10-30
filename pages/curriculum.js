75eb42c295ee6a64dfefd387100212343938e7f5
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-30 22:16:54 +0100

    Update curriculum.js

--- pages/curriculum.js
+++ pages/curriculum.js
@@ -13,7 +13,6 @@ export default function Now() {
       <div>
         <div className="my-2">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">Where am I and what am I doing?</h1>
-          <div className="mt-3 mb-4 text-sm">Last updated: 21 October 2022</div>
         </div>
         {/* Misc */}
         <div className="flex justify-between">
