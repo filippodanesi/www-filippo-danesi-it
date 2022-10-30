03f33b17ed1a16e98a4ecc45e0d55828a9910305
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-30 21:55:27 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,15 +22,14 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Benvenuto/a!
+            Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Ciao a tutti e grazie per aver visitato il mio sito. Mi presento, mi chiamo Filippo Danesi e sono un SEO Specialist.  Il mio lavoro consiste nell'ottimizzare i siti web per i motori di ricerca. In altre parole, mi occupo di migliorare il posizionamento di un sito web sui risultati di ricerca di Google e altri motori di ricerca. Vorrei condividere con voi le mie conoscenze (e alcuni consigli) riguardo questo ambito, quindi se siete appassionati di notizie riguardanti la SEO, il Digital Marketing o il mondo del lavoro (su questa ci provo..) allora siete nel posto giusto!
+            {siteMetadata.description}
           </p>
         </div>
-        </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && 'Nessun post trovato.'}
+          {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
             const { slug, date, title, summary, tags } = frontMatter
             return (
@@ -38,7 +37,7 @@ export default function Home({ posts }) {
                 <article>
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
                     <dl>
-                      <dt className="sr-only">Pubblicato  il</dt>
+                      <dt className="sr-only">Published on</dt>
                       <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date)}</time>
                       </dd>
@@ -70,7 +69,7 @@ export default function Home({ posts }) {
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                           aria-label={`Read "${title}"`}
                         >
-                          Leggi l'articolo &rarr;
+                          Read more &rarr;
                         </Link>
                       </div>
                     </div>
@@ -80,7 +79,6 @@ export default function Home({ posts }) {
             )
           })}
         </ul>
-       </div>
       </div>
       {posts.length > MAX_DISPLAY && (
         <div className="flex justify-end text-base font-medium leading-6">
@@ -89,7 +87,7 @@ export default function Home({ posts }) {
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label="all posts"
           >
-            Tutti i Post &rarr;
+            All Posts &rarr;
           </Link>
         </div>
       )}
