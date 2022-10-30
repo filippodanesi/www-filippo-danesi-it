751ef496392b08f597de7ccf0beeb6fa6eeaac07
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-30 22:20:21 +0100

    Update curriculum.js

--- pages/curriculum.js
+++ pages/curriculum.js
@@ -12,7 +12,9 @@ export default function Now() {
       />
       <div>
         <div className="divide-y divide-gray-200 dark:divide-gray-700">
+          <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">Where am I and what am I doing?</h1>
+          </div>
         </div>
         {/* Misc */}
         <div className="flex justify-between">
