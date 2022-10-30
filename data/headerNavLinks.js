8511ec4962dfce606919df9fc4c621dcc374697f
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-30 16:05:08 +0100

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/chi-sono', title: 'Chi sono' },
+  { href: '/curriculum', title: 'Curriculum' },
 ]
 
 export default headerNavLinks
