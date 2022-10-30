405f66d64cdb163da1e6918058052e824d889a52
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-10-30 16:28:01 +0100

    Update curriculum.js

--- pages/curriculum.js
+++ pages/curriculum.js
@@ -14,7 +14,7 @@ export default function Now() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Benvenuto/a!
+            Curriculum
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             Dove mi trovo e cosa sto facendo? Cosa ho studiato? Qui troverai tutte le info su chi sono e cosa ho fatto per arrivare a lavorare come SEO Specialist e nell'ambito del Digital Marketing!
