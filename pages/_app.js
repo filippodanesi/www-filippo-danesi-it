a0e1dcdcc14fa80142bab68b3512b229ca578333
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-01 18:43:00 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,48 +12,19 @@ import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
-import {useEffect} from 'react'
-import {useRouter} from 'next/router'
-
-import Script from 'next/script'
-
-import * as ga from 'lib/google-tag-manager'
-
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
-  
-    const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-  
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
+      <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
-          <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`} strategy='afterInteractive' />
-          <Script id="google-tag-manager" strategy='afterInteractive' />
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
-          `}
       </LayoutWrapper>
     </ThemeProvider>
   )
