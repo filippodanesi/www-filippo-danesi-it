ef37590b4f617935f85c45a39bf7ed6b767e7c1b
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-02 16:03:44 +0100

    Update curriculum.js

--- pages/curriculum.js
+++ pages/curriculum.js
@@ -31,6 +31,7 @@ export default function Projects() {
   </span>
   <p>Il mio ruolo consiste nell'ottimizzare i siti web per i motori di ricerca. In altre parole, faccio in modo che i siti web vengano visualizzati nei primi risultati di ricerca per le parole chiave appropriate. Ciò può essere ottenuto attraverso diversi metodi, come la creazione di contenuti di alta qualità, il miglioramento del codice del sito e la creazione di link verso altri siti web.</p>
   <br />
+  <br />
   <h2 className="text-3xl font-bold leading-8 text-gray-900 dark:text-gray-100 tracking-tight">
     Ma come sono arrivato a ricoprire il ruolo di SEO Specialist?
   </h2>
