72673484ab10fe5a9fa826f4a1c17863fc513a75
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-02 17:36:43 +0100

    Update curriculum.js

--- pages/curriculum.js
+++ pages/curriculum.js
@@ -28,10 +28,9 @@ export default function Projects() {
       href={'https://www.nur.it'}
       className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-300" 
     >
-      NUR.{' '}
+      NUR.{' '} E sì, mi piace moooooolto :)
     </Link>
   </span>
-  <p> E sì, mi piace moooooolto :) </p>
   <br />
   <p>Il mio ruolo consiste nell'ottimizzare i siti web per i motori di ricerca. In altre parole, faccio in modo che i siti web vengano visualizzati nei primi risultati di ricerca per le parole chiave appropriate. Ciò può essere ottenuto attraverso diversi metodi, come la creazione di contenuti di alta qualità, il miglioramento del codice del sito e la creazione di link verso altri siti web.</p>
   <br />
