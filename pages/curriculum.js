8d8d792e478c897c2bc815d0fe0b960e8c56ddba
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-03 11:34:25 +0100

    Update curriculum.js

--- pages/curriculum.js
+++ pages/curriculum.js
@@ -5,7 +5,7 @@ import { PageSEO } from '@/components/SEO'
 export default function Projects() {
   return (
     <>
-      <PageSEO title={`Lavoro come SEO Specialist e Digital Strategist - ${siteMetadata.author}`} description={`Dove sono e cosa sto facendo? Cosa ho studiato? Qui troverai tutte le informazioni su come sono arrivato a svolgere questo lavoro e cosa mi ha portato a diventare un SEO Specialist!`} />
+      <PageSEO title={`Lavoro come SEO Specialist e Digital Strategist - ${siteMetadata.author}`} description={`Qui troverai tutte le informazioni su come sono arrivato a svolgere questo lavoro e cosa mi ha portato a diventare un SEO Specialist!`} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
