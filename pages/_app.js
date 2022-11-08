47e6d0c75c35dc7d36758138b6f4ae53706607c2
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-08 13:18:09 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,27 +12,6 @@ import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import TagManager from 'react-gtm-module';
-import Users from './Users'
-import Posts from './Posts'
-import Login from './Login';
-const App = () => {
-  useEffect(() => {
-	TagManager.initialize({ gtmId: 'GTM-XXXXX' });
-  }, []);
-  return (
-      <BrowserRouter>
-        <React.Fragment>
-          <Route path="/" component={Users} exact />
-          <Route path="/posts" component={Posts} exact />
-          <Route path="/login" component={Login} exact />
-        </React.Fragment>
-      </BrowserRouter>
-  );
-}
-
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
