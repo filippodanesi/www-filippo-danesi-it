034f574b23854dc9cdf1cd68c8286e621253eb9e
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-08 08:59:35 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -23,8 +23,19 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+          <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M8V2TNP');</script>
+          <!-- End Google Tag Manager -->
         </Head>
         <body className="bg-[#F9FAFB] text-black antialiased dark:bg-gray-900 dark:text-white">
+          <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8V2TNP"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->
           <Main />
           <NextScript />
         </body>
