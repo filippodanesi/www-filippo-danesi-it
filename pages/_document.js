86d33c7e5f20e7fc5fb0f7e0282a125ae6507208
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-09 13:29:51 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -23,41 +23,15 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      
-            <script>
-              (function (w, d, s, l, i) {
-                w[l] = w[l] || [];
-                w[l].push({"gtm.start": new Date().getTime(), event: "gtm.js"});
-                var f = d.getElementsByTagName(s)[0],
-                  j = d.createElement(s),
-                  dl = l != "dataLayer" ? "&l=" + l : "";
-                j.async = true;
-                j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-                f.parentNode.insertBefore(j, f);
-              })(
-                window,
-                document,
-                "script",
-                "dataLayer",
-                "GTM-M8V2TNP",
-              );
-            </script>
-
         </Head>
         <body className="bg-[#F9FAFB] text-black antialiased dark:bg-gray-900 dark:text-white">
-          
           <Main />
           <NextScript />
-
             <noscript
-              ><iframe
-                src="https://www.googletagmanager.com/ns.html?id=GTM-M8V2TNP"
-                height="0"
-                width="0"
-                style="display:none;visibility:hidden"
-              ></iframe
-            ></noscript>
-
+              dangerouslySetInnerHTML={{
+                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8V2TNP" height="0" width="0" style="display: none; visibility: hidden;" />`,
+              }}
+            />
         </body>
       </Html>
     )
