c5a3c3d730d2ed682c87f536d300c202010408c5
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-17 16:23:12 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -163,60 +163,7 @@ module.exports = {
           },
         },
       }),
-      keyframes: {
-        'bg-hue-animation': {
-          '0%': { filter: 'hue-rotate(0deg)' },
-          '50%': { filter: 'hue-rotate(180deg)' },
-          '100%': { filter: 'hue-rotate(0deg)' },
-        },
-        'fade-away': {
-          '0%': {
-            opacity: 1,
-          },
-          '100%': {
-            opacity: 0.2,
-          },
-        },
-        shrink: {
-          '0% , 100%': {
-            height: '0.75rem',
-          },
-          '50%': {
-            height: '0.375rem',
-          },
-        },
-        expand: {
-          '0% , 100%': {
-            height: '0.375rem',
-          },
-          '50%': {
-            height: '0.75rem',
-          },
-        },
-      },
-      animation: {
-        'fade-text': '10s ease-in-out 3s 1 normal forwards running fade-away',
-        shrink: 'shrink ease-in-out 1.5s infinite',
-        expand: 'expand ease-in-out 1.5s infinite',
-        'hue-animation': 'bg-hue-animation 10s infinite',
-      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    plugin(function ({ addVariant, e, postcss }) {
-      addVariant('firefox', ({ container, separator }) => {
-        const isFirefoxRule = postcss.atRule({
-          name: '-moz-document',
-          params: 'url-prefix()',
-        })
-        isFirefoxRule.append(container.nodes)
-        container.append(isFirefoxRule)
-        isFirefoxRule.walkRules((rule) => {
-          rule.selector = `.${e(`firefox${separator}${rule.selector.slice(1)}`)}`
-        })
-      })
-    }),
-  ],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
