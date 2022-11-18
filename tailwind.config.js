77413da8edfeee1a9270327c61e2c7f3f4edb89e
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-18 12:24:12 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,22 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-const plugin = require('tailwindcss/plugin')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  experimental: {
+    optimizeUniversalDefaults: true,
+  },
   content: [
     './pages/**/*.js',
     './components/**/*.js',
     './layouts/**/*.js',
     './lib/**/*.js',
-    './context/**/*.js',
+    './data/**/*.mdx',
   ],
   darkMode: 'class',
   theme: {
     extend: {
-      fontSize: {
-        '6xl': '3.4rem',
-      },
       spacing: {
         '9/16': '56.25%',
-        0.75: '0.1875rem',
       },
       lineHeight: {
         11: '2.75rem',
@@ -26,34 +24,12 @@ module.exports = {
         13: '3.25rem',
         14: '3.5rem',
       },
-      backgroundImage: {
-        'texture-pattern': "url('/static/images/right-dark-gradient.svg')",
-      },
       fontFamily: {
-        sans: ['Be Vietnam Pro', ...defaultTheme.fontFamily.sans],
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: colors.blue,
         gray: colors.slate,
-        violet: {
-          ...colors.blue,
-          30: '#f9f4fc',
-          950: '#170a2d',
-          1000: '#120724',
-        },
-        green: colors.emerald,
-        yellow: colors.amber,
-        purple: colors.violet,
-        'spotify-green': '#1DB954',
-      },
-      hueRotate: {
-        53: '53deg',
-      },
-      saturate: {
-        1000: '10',
-      },
-      zIndex: {
-        '-1': '-1',
       },
       typography: (theme) => ({
         DEFAULT: {
@@ -83,6 +59,9 @@ module.exports = {
             'h4,h5,h6': {
               color: theme('colors.gray.900'),
             },
+            pre: {
+              backgroundColor: theme('colors.gray.800'),
+            },
             code: {
               color: theme('colors.pink.500'),
               backgroundColor: theme('colors.gray.100'),
@@ -92,10 +71,10 @@ module.exports = {
               paddingBottom: '2px',
               borderRadius: '0.25rem',
             },
-            'code:before': {
+            'code::before': {
               content: 'none',
             },
-            'code:after': {
+            'code::after': {
               content: 'none',
             },
             details: {
@@ -107,11 +86,11 @@ module.exports = {
               borderRadius: '0.25rem',
             },
             hr: { borderColor: theme('colors.gray.200') },
-            'ol li:before': {
+            'ol li::marker': {
               fontWeight: '600',
               color: theme('colors.gray.500'),
             },
-            'ul li:before': {
+            'ul li::marker': {
               backgroundColor: theme('colors.gray.500'),
             },
             strong: { color: theme('colors.gray.600') },
@@ -148,6 +127,9 @@ module.exports = {
             'h4,h5,h6': {
               color: theme('colors.gray.100'),
             },
+            pre: {
+              backgroundColor: theme('colors.gray.800'),
+            },
             code: {
               backgroundColor: theme('colors.gray.800'),
             },
@@ -155,11 +137,11 @@ module.exports = {
               backgroundColor: theme('colors.gray.800'),
             },
             hr: { borderColor: theme('colors.gray.700') },
-            'ol li:before': {
+            'ol li::marker': {
               fontWeight: '600',
               color: theme('colors.gray.400'),
             },
-            'ul li:before': {
+            'ul li::marker': {
               backgroundColor: theme('colors.gray.400'),
             },
             strong: { color: theme('colors.gray.100') },
@@ -180,60 +162,7 @@ module.exports = {
           },
         },
       }),
-      keyframes: {
-        'bg-hue-animation': {
-          '0%': { filter: 'hue-rotate(0deg)' },
-          '50%': { filter: 'hue-rotate(180deg)' },
-          '100%': { filter: 'hue-rotate(0deg)' },
-        },
-        'fade-away': {
-          '0%': {
-            opacity: 1,
-          },
-          '100%': {
-            opacity: 0.2,
-          },
-        },
-        shrink: {
-          '0% , 100%': {
-            height: '0.75rem',
-          },
-          '50%': {
-            height: '0.375rem',
-          },
-        },
-        expand: {
-          '0% , 100%': {
-            height: '0.375rem',
-          },
-          '50%': {
-            height: '0.75rem',
-          },
-        },
-      },
-      animation: {
-        'fade-text': '10s ease-in-out 3s 1 normal forwards running fade-away',
-        shrink: 'shrink ease-in-out 1.5s infinite',
-        expand: 'expand ease-in-out 1.5s infinite',
-        'hue-animation': 'bg-hue-animation 10s infinite',
-      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    plugin(function ({ addVariant, e, postcss }) {
-      addVariant('firefox', ({ container, separator }) => {
-        const isFirefoxRule = postcss.atRule({
-          name: '-moz-document',
-          params: 'url-prefix()',
-        })
-        isFirefoxRule.append(container.nodes)
-        container.append(isFirefoxRule)
-        isFirefoxRule.walkRules((rule) => {
-          rule.selector = `.${e(`firefox${separator}${rule.selector.slice(1)}`)}`
-        })
-      })
-    }),
-  ],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
