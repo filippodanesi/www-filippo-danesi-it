e8597a7eb147fb8da78a647257bd8f509a62e3fd
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-20 17:50:31 +0100

    Update curriculum.js

--- pages/curriculum.js
+++ pages/curriculum.js
@@ -1,21 +1,49 @@
-import { MDXLayoutRenderer } from '@/components/MDXComponents'
-import { getFileBySlug } from '@/lib/mdx'
+import useTranslation from 'next-translate/useTranslation'
+import { getAllFilesFrontMatter, getFileBySlug } from '@/lib/mdx'
+import siteMetadata from '@/data/siteMetadata'
+import ListLearningLayout from '@/layouts/ListLearningLayout'
+import { PageSEO } from '@/components/SEO'
 
-const DEFAULT_LAYOUT = 'AuthorLayout'
+export const POSTS_PER_PAGE = 1
 
-export async function getStaticProps() {
-  const authorDetails = await getFileBySlug('authors', ['curriculum'])
-  return { props: { authorDetails } }
+export async function getStaticProps({ locale, defaultLocale, locales }) {
+  const otherLocale = locale !== defaultLocale ? locale : ''
+  const posts = await getAllFilesFrontMatter('learning', otherLocale)
+  const post = await getFileBySlug('learning', posts[0].slug, otherLocale)
+  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
+  const pagination = {
+    currentPage: 1,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+  }
+
+  return {
+    props: { initialDisplayPosts, posts, post, pagination, locale, availableLocales: locales },
+  }
 }
 
-export default function About({ authorDetails }) {
-  const { mdxSource, frontMatter } = authorDetails
-
+export default function Learning({
+  posts,
+  post,
+  initialDisplayPosts,
+  pagination,
+  locale,
+  availableLocales,
+}) {
+  const { t } = useTranslation()
   return (
-    <MDXLayoutRenderer
-      layout={frontMatter.layout || DEFAULT_LAYOUT}
-      mdxSource={mdxSource}
-      frontMatter={frontMatter}
-    />
+    <>
+      <PageSEO
+        title={`${t('common:learning')} - ${siteMetadata.author}`}
+        description={siteMetadata.description[locale]}
+        availableLocales={availableLocales}
+      />
+      <ListLearningLayout
+        posts={posts}
+        post={post}
+        initialDisplayPosts={initialDisplayPosts}
+        pagination={pagination}
+        title={t('common:learning')}
+      />
+    </>
   )
 }
