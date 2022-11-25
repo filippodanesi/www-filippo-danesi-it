2cecbc7518f453ad76286e1ab1680a94545d67f4
Filippo Danesi <50838949+filippodanesi@users.noreply.github.com>
2022-11-25 23:06:40 +0100

    Update CurriculumLayout.js

--- layouts/CurriculumLayout.js
+++ layouts/CurriculumLayout.js
@@ -7,11 +7,11 @@ export default function AuthorLayout({ children, frontMatter }) {
 
   return (
     <>
-      <PageSEO title={`Sono un SEO Specialist e Digital Strategist - ${name}`} description={`Ciao! Mi chiamo Filippo Danesi (sì l'ho detto molte volte) e mi occupo di ottimizzazione sui motori di ricerca (SEO) e strategie di Digital Marketing.`} />
+      <PageSEO title={`Lavoro come SEO Specialist da più di due anni - ${name}`} description={`Ciao! Mi chiamo Filippo Danesi (sì l'ho detto molte volte) e mi occupo di ottimizzazione sui motori di ricerca (SEO) e strategie di Digital Marketing.`} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Chi sono
+            Curriculum
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
